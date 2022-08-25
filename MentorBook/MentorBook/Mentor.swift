ff28516921703b9b7f665d098e0c052b2887a153
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-26 08:16:55 +0900

    final

---
+++ MentorBook/MentorBook/Mentor.swift
@@ -0,0 +1,24 @@
+//
+//  Mentor.swift
+//  MentorBook
+//
+//  Created by K I on 2022/08/25.
+//
+
+import UIKit
+
+class Mentor{
+    var name:String!
+    var course:String!
+    var imageName:String!
+    
+    init(name:String,imageName:String,course:String){
+        self.name=name
+        self.imageName=imageName
+        self.course=course
+    }
+    
+    func getImage()->UIImage{
+        return UIImage(named:imageName)!
+    }
+}
